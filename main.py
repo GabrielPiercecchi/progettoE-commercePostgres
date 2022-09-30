95dfd9913d74f9e884c7936419d6c675f7e13355
GabrielP <S1097612@studenti.univpm.it>
2022-09-30 15:08:44 +0200

    Commit Setup

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
-
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from fastapi import FastAPI
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = FastAPI()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.get("/")
+async def root():
+    return {"message": "Hello PyCharm "}
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@app.get("/hello/{name}")
+async def say_hello(name: int):
+    return {"message": f"Hello {name}"}
