c5566d8570643a392bb1600f540e63952c0eda4f
GabrielP <S1097612@studenti.univpm.it>
2022-09-30 18:09:46 +0200

    Commit Setup

--- ecommerce/config.py
+++ ecommerce/config.py
@@ -1,9 +1,9 @@
 import os
 
 APP_ENV = os.getenv('APP_ENV', 'development')
-DATABASE_USERNAME = os.getenv('DATABASE_USERNAME', 'postgres')
-DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', 'mukul123')
+DATABASE_USERNAME = os.getenv('DATABASE_USERNAME', 'gabriel')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', 'StrongPassword')
 DATABASE_HOST = os.getenv('DATABASE_HOST', 'localhost')
-DATABASE_NAME = os.getenv('DATABASE_NAME', 'ecommerce')
-TEST_DATABASE_NAME = os.getenv('DATABASE_NAME', 'ecommerce_test')
+DATABASE_NAME = os.getenv('DATABASE_NAME', 'postgres')
+TEST_DATABASE_NAME = os.getenv('DATABASE_NAME', 'postgres@localhost')
 
