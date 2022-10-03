bce5d952f2ef13504f674e793500c10fe5293002
GabrielP <S1097612@studenti.univpm.it>
2022-10-03 20:07:38 +0200

    Setup Database

--- ecommerce/cart/router.py
+++ ecommerce/cart/router.py
@@ -6,7 +6,6 @@ from ecommerce.user.schema import User
 from . import services
 from . import schema
 
-
 router = APIRouter(
     tags=["Cart"],
     prefix='/cart'
@@ -17,3 +16,14 @@ router = APIRouter(
 async def add_product_to_cart(product_id: int, database: Session = Depends(db.get_db)):
     result = await services.add_to_cart(product_id, database)
     return result
+
+
+@router.get('/', response_model=schema.ShowCart)
+async def get_all_cart_items(database: Session = Depends(db.get_db)):
+    result = await services.get_all_items(database)
+    return result
+
+
+@router.delete('/{cart_item_id}', status_code=status.HTTP_204_NO_CONTENT, response_class=Response)
+async def remove_cart_item_by_id(cart_item_id: int, database: Session = Depends(db.get_db)):
+    await services.remove_cart_item(cart_item_id, database)
--- ecommerce/cart/services.py
+++ ecommerce/cart/services.py
@@ -4,7 +4,9 @@ from sqlalchemy.orm import Session
 from ecommerce import db
 from ecommerce.products.models import Product
 from ecommerce.user.models import User
-from . models import Cart, CartItems
+from .models import Cart, CartItems
+from . import schema
+from .schema import ShowCart
 
 
 async def add_items(cart_id, product_id, database: Session = Depends(db.get_db)):
@@ -35,3 +37,18 @@ async def add_to_cart(product_id, database: Session = Depends(db.get_db)):
     else:
         await add_items(cart_info.id, product_info.id, database)
     return {"status": "Item Added to Cart"}
+
+
+async def get_all_items(database) -> ShowCart:
+    user_info = database.query(User).filter(User.email == "elon@tesla.com").first()
+    cart = database.query(Cart).filter(Cart.user_id == user_info.id).first()
+    return cart
+
+
+async def remove_cart_item(cart_item_id, database) -> None:
+    user_info = database.query(User).filter(User.email == "elon@tesla.com").first()
+    cart_id = database.query(Cart).filter(User.id == user_info.id).first()
+    database.query(CartItems).filter(CartItems.id == cart_item_id,
+                                     CartItems.cart_id == cart_id.id).delete()
+    database.commit()
+    return
