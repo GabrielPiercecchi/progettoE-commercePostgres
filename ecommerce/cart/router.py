07df58fb168f15d5bf200154a526a3f3903bcefa
GabrielP <S1097612@studenti.univpm.it>
2022-10-03 22:09:40 +0200

    Setup Database

--- ecommerce/cart/router.py
+++ ecommerce/cart/router.py
@@ -1,3 +1,5 @@
+import decimal
+
 from fastapi import APIRouter, Depends, status, Response
 from sqlalchemy.orm import Session
 
@@ -13,17 +15,34 @@ router = APIRouter(
 
 
 @router.post('/add', status_code=status.HTTP_201_CREATED)
-async def add_product_to_cart(product_id: int, database: Session = Depends(db.get_db)):
-    result = await services.add_to_cart(product_id, database)
+async def add_product_to_cart(product_id: int, user_id: int, database: Session = Depends(db.get_db)):
+    result = await services.add_to_cart_by_id(product_id, user_id, database)
     return result
+
+
+# @router.post('/add', status_code=status.HTTP_201_CREATED)
+# async def add_product_to_cart(product_id: int, database: Session = Depends(db.get_db)):
+#     result = await services.add_to_cart(product_id, database)
+#     return result
+
+
+# @router.get('/', response_model=schema.ShowCart)
+# async def get_all_cart_items(database: Session = Depends(db.get_db)):
+#     result = await services.get_all_items(database)
+#     return result
 
 
 @router.get('/', response_model=schema.ShowCart)
-async def get_all_cart_items(database: Session = Depends(db.get_db)):
-    result = await services.get_all_items(database)
+async def get_all_cart_items_by_id(user_id: int, database: Session = Depends(db.get_db)):
+    result = await services.get_all_items_id(user_id, database)
     return result
 
 
+# @router.delete('/{cart_item_id}', status_code=status.HTTP_204_NO_CONTENT, response_class=Response)
+# async def remove_cart_item_by_id(cart_item_id: int, database: Session = Depends(db.get_db)):
+#     await services.remove_cart_item(cart_item_id, database)
+
+
 @router.delete('/{cart_item_id}', status_code=status.HTTP_204_NO_CONTENT, response_class=Response)
-async def remove_cart_item_by_id(cart_item_id: int, database: Session = Depends(db.get_db)):
-    await services.remove_cart_item(cart_item_id, database)
+async def remove_cart_item_by_email(cart_item_id: int, database: Session = Depends(db.get_db)):
+    await services.remove_cart_item_email(cart_item_id, database)
--- ecommerce/cart/services.py
+++ ecommerce/cart/services.py
@@ -1,11 +1,14 @@
+from typing import List
+
 from fastapi import HTTPException, status, Depends
 from sqlalchemy.orm import Session
 
+import ecommerce
 from ecommerce import db
 from ecommerce.products.models import Product
 from ecommerce.user.models import User
 from .models import Cart, CartItems
-from . import schema
+from . import schema, models
 from .schema import ShowCart
 
 
@@ -16,17 +19,17 @@ async def add_items(cart_id, product_id, database: Session = Depends(db.get_db))
     database.refresh(cart_items)
 
 
-async def add_to_cart(product_id, database: Session = Depends(db.get_db)):
+async def add_to_cart_by_id(product_id, user_id, database: Session = Depends(db.get_db)):
+    user_info = database.query(ecommerce.user.models.User).get(user_id)
     product_info = database.query(Product).get(product_id)
-    if not product_id:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Data Not Found !")
+    cart_info = database.query(Cart).filter(Cart.user_id == user_info.id).first()
 
+    if not user_info:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User Not Found !")
+    if not product_id:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product Not Found !")
     if product_info.quantity <= 0:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Items Out of Stock !")
-
-    user_info = database.query(User).filter(User.email == "elon@tesla.com").first()
-
-    cart_info = database.query(Cart).filter(Cart.user_id == user_info.id).first()
 
     if not cart_info:
         new_cart = Cart(user_id=user_info.id)
@@ -39,16 +42,56 @@ async def add_to_cart(product_id, database: Session = Depends(db.get_db)):
     return {"status": "Item Added to Cart"}
 
 
-async def get_all_items(database) -> ShowCart:
-    user_info = database.query(User).filter(User.email == "elon@tesla.com").first()
+# async def add_to_cart(product_id, database: Session = Depends(db.get_db)):
+#     product_info = database.query(Product).get(product_id)
+#     if not product_id:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Data Not Found !")
+#
+#     if product_info.quantity <= 0:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Items Out of Stock !")
+#
+#     user_info = database.query(User).filter(User.email == "elon@tesla.com").first()
+#
+#     cart_info = database.query(Cart).filter(Cart.user_id == user_info.id).first()
+#
+#     if not cart_info:
+#         new_cart = Cart(user_id=user_info.id)
+#         database.add(new_cart)
+#         database.commit()
+#         database.refresh(new_cart)
+#         await add_items(new_cart.id, product_info.id, database)
+#     else:
+#         await add_items(cart_info.id, product_info.id, database)
+#     return {"status": "Item Added to Cart"}
+
+
+# async def get_all_items(database) -> ShowCart:
+#     user_info = database.query(User).filter(User.email == "elon@tesla.com").first()
+#     cart = database.query(Cart).filter(Cart.user_id == user_info.id).first()
+#     return cart
+
+
+async def get_all_items_id(user_id, database) -> ShowCart:
+    user_info = database.query(ecommerce.user.models.User).get(user_id)
+    if not user_info:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Data Not Found !")
     cart = database.query(Cart).filter(Cart.user_id == user_info.id).first()
     return cart
 
 
-async def remove_cart_item(cart_item_id, database) -> None:
-    user_info = database.query(User).filter(User.email == "elon@tesla.com").first()
-    cart_id = database.query(Cart).filter(User.id == user_info.id).first()
-    database.query(CartItems).filter(CartItems.id == cart_item_id,
-                                     CartItems.cart_id == cart_id.id).delete()
+# async def remove_cart_item(cart_item_id, database) -> None:
+#     user_info = database.query(User).filter(User.email == "elon@tesla.com").first()
+#     cart_id = database.query(Cart).filter(User.id == user_info.id).first()
+#     database.query(CartItems).filter(CartItems.id == cart_item_id,
+#                                      CartItems.cart_id == cart_id.id).delete()
+#     database.commit()
+#     return
+
+
+async def remove_cart_item_email(cart_item_id, database) -> None:
+    # user_info = database.query(User).filter(User.email == user_email).first()
+    # cart_id = database.query(Cart).filter(User.id == user_info.id).first()
+
+    database.query(CartItems).filter(CartItems.id == cart_item_id).delete()
     database.commit()
     return
