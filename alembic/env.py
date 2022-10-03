12087a5e715c8c5f77d0ea9c6d77f6ceb2784c03
GabrielP <S1097612@studenti.univpm.it>
2022-10-03 14:38:12 +0200

    Setup Database

--- alembic/env.py
+++ alembic/env.py
@@ -16,6 +16,7 @@ fileConfig(config.config_file_name)
 from ecommerce import config as config_venv
 from ecommerce.db import Base
 from ecommerce.user.models import User
+from ecommerce.products.models import Product, Category
 
 # add your model's MetaData object here
 # for 'autogenerate' support
