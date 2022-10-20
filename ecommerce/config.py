911898d5a61c6faf2391d333cef4e0f471aeee3d
GabrielP <S1097612@studenti.univpm.it>
2022-10-20 16:25:17 +0200

    Setup Database Security

--- ecommerce/config.py
+++ ecommerce/config.py
@@ -1,4 +1,6 @@
 import os
+import mysql.connector
+
 
 APP_ENV = os.getenv('APP_ENV', 'development')
 DATABASE_USERNAME = os.getenv('DATABASE_USERNAME', 'gabriel')
@@ -6,4 +8,13 @@ DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', 'StrongPassword')
 DATABASE_HOST = os.getenv('DATABASE_HOST', 'localhost')
 DATABASE_NAME = os.getenv('DATABASE_NAME', 'postgres')
 TEST_DATABASE_NAME = os.getenv('DATABASE_NAME', 'postgres')
+
+
+# mydb = mysql.connector.connect(
+#     host="localhost",
+#     user="gabriel",
+#     password="StrongPassword",
+#     database="basidb"
+# )
+
 # TEST_DATABASE_NAME = os.getenv("DATABASE_NAME", "ecommerce_test")
